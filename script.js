349c58333b2972891e961be1cc5b2f5a9de7297f
chrissy-t <86742207+chrissy-t@users.noreply.github.com>
2021-12-14 00:43:58 -0400

    nothing

---
+++ script.js
@@ -0,0 +1,10 @@
+/*A small business wishes to display a bill for its customers on a web page.
+ Design a web page which includes three items along with their prices.
+ The bill should include the sub-total, VAT and total charge.
+As it is the holiday season, the business also wishes to include a 5% discount on the sub-total.
+Each item’s price, the VAT, the discount and the total should have unique IDs for their respective HTML element.
+ Write JavaScript which will read the prices from the webpage and store the sum in a sub-total variable.
+ Calculate the discount, VAT and total charge and store in separate variables. Update the web page with the calculated values*/
+
+   var 'cost1'= document.getElementById('cost1');
+   var 
